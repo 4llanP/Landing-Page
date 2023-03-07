4ec271e08b5f221cebc42b9ef1970345d8124be3
All4nP <66078686+4llanP@users.noreply.github.com>
2023-03-06 21:59:31 -0300

    fiz algumas midificacoes e criei um espaco para ampliacao

--- Landing page (HTML CSS)/scripts.js
+++
@@ -1,6 +0,0 @@
-var box = document.querySelector('.box');
-
-document.addEventListener('mousemove', function(event) {
-  box.style.left = event.clientX + 'px';
-  box.style.top = event.clientY + 'px';
-});
